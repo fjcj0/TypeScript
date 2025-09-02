9b381d4b9a260ec6dc83151bfc55113e7a936469
omar <omarqais225@gmail.com>
2025-09-02 23:10:42 +0300

    default values

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const showMsg = true;
-const showDetails = (name, age, salary) => {
+const showDetails = (name = "Default Name", age, salary) => {
     if (showMsg) {
         return `Hello ${name}, Your Age is ${age}, Your Salary is ${salary}`;
     }
     return false;
 };
-console.log(showDetails("Jack", 20, 20.5));
+console.log(showDetails(undefined, 20, 20.5));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 const showMsg: boolean = true;
 
-const showDetails = (name:string,age:number,salary:number):any => {
+const showDetails = (name:string = "Default Name",age:number,salary:number):any => {
     if (showMsg) {
         return `Hello ${name}, Your Age is ${age}, Your Salary is ${salary}`;
     }
     return false;
 };
-console.log(showDetails("Jack",20,20.5));
+console.log(showDetails(undefined,20,20.5));
