e9324a64879a4c35828d64eb7c2a5fc68587a611
omar <omarqais225@gmail.com>
2025-09-02 23:01:32 +0300

    arary datatypes

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let Name = "PsCoding";
-let ExperinceYear = 5;
-let hire = false;
-let all = "This accept all data type";
-const sum = (number1, number2) => {
-    return number1 + number2;
-};
-console.log(sum(10, 20));
+let all = "Hello world!!";
+console.log(all);
+all = 1;
+console.log(all);
+const letMyFriends = ["Ali", "Mohammed", "Adam", "Jack", "Fadi", 10, true, false];
+letMyFriends.map((friend, index) => {
+    console.log((index + 1) + ': ' + friend);
+});
+const twoArray = [1, 2, 3, 4, ['Fadi', 'Ahmed'], [1, 2, 3, 4, 5, 6], ['Jack', 'John'], [10, 11, 12, 13], false, true];
+twoArray.forEach((item, index) => {
+    if (Array.isArray(item)) {
+        console.log(`${index + 1}: [${item.join(', ')}]`);
+    }
+    else {
+        console.log(`${index + 1}: ${item}`);
+    }
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
-let Name: string = "PsCoding";
-let ExperinceYear: number = 5;
-let hire: boolean = false;
-let all: any = "This accept all data type";
-const sum = (number1: number, number2: number):number => {
-    return number1 + number2;
-};
-console.log(sum(10, 20));
+let all: string | number | boolean = "Hello world!!";
+
+console.log(all);
+
+all = 1;
+
+console.log(all);
+
+const letMyFriends: (string | number | boolean)[] = ["Ali", "Mohammed", "Adam", "Jack", "Fadi", 10,true,false];
+
+letMyFriends.map((friend, index) => {
+    console.log((index + 1) + ': ' + friend);
+});
+
+const twoArray: (string | number | boolean | string[] | number[])[] = [1, 2, 3, 4, ['Fadi', 'Ahmed'], [1, 2, 3, 4, 5, 6], ['Jack', 'John'], [10, 11, 12, 13], false, true];
+
+twoArray.forEach((item, index) => {
+    if (Array.isArray(item)) {
+        console.log(`${index + 1}: [${item.join(', ')}]`);
+    } else {
+        console.log(`${index + 1}: ${item}`);
+    }
+});
