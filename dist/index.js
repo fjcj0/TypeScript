1f6ccc9b8f7980a34ea588811c01f7fe0552c0c6
omar <omarqais225@gmail.com>
2025-09-02 22:51:35 +0300

    datatype function

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,8 @@ let Name = "PsCoding";
 let ExperinceYear = 5;
 let hire = false;
 let all = "This accept all data type";
+const sum = (number1, number2) => {
+    console.log(number1 + number2);
+};
+sum(10, 20);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ let Name: string = "PsCoding";
 let ExperinceYear: number = 5;
 let hire: boolean = false;
 let all: any = "This accept all data type";
+const sum = (number1: number, number2: number) => {
+    console.log(number1 + number2);
+};
+sum(10, 20);
