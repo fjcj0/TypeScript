83f0beb033bff731941abcc642b09031d56dca06
omar <omarqais225@gmail.com>
2025-09-02 23:48:17 +0300

    type alias advanced

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const teacher = "Jack Johnson";
-const students = [1, 2, 3, 4, "ahmed", true, false, "ali"];
-console.log("Teacher name: " + teacher);
-console.log("his students number and name: ");
-students.map((student, index) => (console.log((index + 1) + ': ' + student)));
+const getActions = (btns) => {
+    console.log(`Up: ${btns.up} ,Down: ${btns.down} ,Right: ${btns.right} ,left: ${btns.left}`);
+    btns.caps.map((cap, index) => (console.log(cap)));
+    btns.esc.map((es, index) => (console.log(es)));
+};
+getActions({ up: "go up", down: "go down", right: 1, left: 2, caps: [1, 2, 3, 4, 5, 6], esc: ["jack", "johnson", "liz", false, true, "kane"] });
+const getNewActions = (btns) => {
+    console.log(`Up: ${btns.up} ,Down: ${btns.down} ,Right: ${btns.right} ,left: ${btns.left}`);
+    btns.caps.map((cap, index) => (console.log(cap)));
+    btns.esc.map((es, index) => (console.log(es)));
+    console.log('x: ', btns.x);
+};
+getNewActions({ up: "go up", down: "go down", right: 1, left: 2, caps: [1, 2, 3, 4, 5, 6], esc: ["jack", "johnson", "liz", false, true, "kane"], x: true });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,36 @@
-type s = string;
-const teacher: s = "Jack Johnson";
-type college = string | number | (number | string | boolean) [];
-const students: college = [1, 2, 3, 4, "ahmed", true, false, "ali"];
-console.log("Teacher name: " + teacher);
-console.log("his students number and name: ");
-students.map((student, index) => (
-    console.log((index+1) + ': ' + student) 
-));
+type buttons = {
+    up: string,
+    down: string,
+    left: number,
+    right: number,
+    caps: number[],
+    esc: (string | boolean) [],
+};
+const getActions  = (btns:buttons) => {
+    console.log(`Up: ${btns.up} ,Down: ${btns.down} ,Right: ${btns.right} ,left: ${btns.left}`);
+    btns.caps.map((cap, index) => (
+        console.log(cap)
+    ));
+       btns.esc.map((es, index) => (
+        console.log(es)
+    ));
+};
+
+getActions({ up: "go up", down: "go down", right: 1, left: 2, caps: [1, 2, 3, 4, 5, 6], esc: ["jack", "johnson", "liz", false, true, "kane"] });
+
+type last = buttons & {
+x: boolean,
+};
+
+const getNewActions  = (btns:last) => {
+    console.log(`Up: ${btns.up} ,Down: ${btns.down} ,Right: ${btns.right} ,left: ${btns.left}`);
+    btns.caps.map((cap, index) => (
+        console.log(cap)
+    ));
+       btns.esc.map((es, index) => (
+        console.log(es)
+       ));
+    console.log('x: ', btns.x);
+};
+
+getNewActions({ up: "go up", down: "go down", right: 1, left: 2, caps: [1, 2, 3, 4, 5, 6], esc: ["jack", "johnson", "liz", false, true, "kane"] ,x:true});
