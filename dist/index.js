ac45ccf09117092d6fd4b13868acf067f27a3d5d
omar <omarqais225@gmail.com>
2025-09-03 18:54:16 +0300

    convert data using as

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const kids = 15;
-const easy = 9;
-const medium = 6;
-const hard = 3;
-var level;
-(function (level) {
-    level[level["kids"] = 15] = "kids";
-    level[level["easy"] = 9] = "easy";
-    level[level["medium"] = 6] = "medium";
-    level[level["hard"] = 3] = "hard";
-})(level || (level = {}));
-;
-let lvl = "easy";
-if (lvl === 'easy') {
-    console.log(`The level is ${lvl} and number of seconds is ${level.easy}s`);
-}
+let data = "1000";
+console.log(data.repeat(3));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,2 @@
-const kids:number = 15;
-const easy: number = 9;
-const medium: number = 6;
-const hard: number = 3;
-enum level {
-    kids = 15,
-    easy = 9,
-    medium = 6,
-    hard = 3
-};
-let lvl: string = "easy";
-if (lvl === 'easy') {
-    console.log(`The level is ${lvl} and number of seconds is ${level.easy}s`);
-}
+let data: any = "1000";
+console.log((data as string).repeat(3));
