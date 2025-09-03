8fc6d8113bcde3a507ebd8dadaebc6860e50f368
omar <omarqais225@gmail.com>
2025-09-03 21:24:28 +0300

    mutiple generic types

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const returnType = (val) => {
-    return val;
+const multipleTypes = (firstValue, secondValue) => {
+    return `The first value is: ${firstValue}, The second value is: ${secondValue}`;
 };
-console.log(returnType(100));
-console.log(returnType("Jack"));
-console.log(returnType(true));
+console.log(multipleTypes(100, 100));
+console.log(multipleTypes('jack', 100));
+console.log(multipleTypes(100, [100, 200, 300, 400]));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-const returnType = <GenericType>(val: GenericType): GenericType => {
-    return val;
-};
-console.log(returnType<number>(100));
-console.log(returnType<string>("Jack"));
-console.log(returnType<boolean>(true));
+const multipleTypes = <FirstType, SecondType>(firstValue: FirstType, secondValue: SecondType): string => {
+    return `The first value is: ${firstValue}, The second value is: ${secondValue}`;
+}
+console.log(multipleTypes<number, number>(100, 100));
+console.log(multipleTypes<string, number>('jack', 100));
+console.log(multipleTypes<number, number[]>(100, [100, 200, 300, 400])); 
